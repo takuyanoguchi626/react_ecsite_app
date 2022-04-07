b2778fb05afe310e779e7673ac8f5c4925fe5768
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-07 15:09:57 +0900

    クラス図完成

---
+++ src/types/UserInfo.ts
@@ -0,0 +1,12 @@
+export type UserInfo = {
+  name?: string;
+  mailAddress?: string;
+  password?: number;
+  zipCode?: number;
+  address?: string;
+  telephone?: number;
+  itemTotalPrice?: number;
+  deliveryDate?: Date;
+  deliveryHour?: number;
+  paymentMethod?: number;
+};
