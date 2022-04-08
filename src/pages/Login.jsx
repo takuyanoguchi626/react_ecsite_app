6d78546f7dd517817a316040bc99edfa82ceedc6
kanacoyn <knkyn723@gmail.com>
2022-04-08 11:31:34 +0900

    ログイン機能を実装

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
-import { UserInfo } from "../types/UserInfo";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Login = () => {
-  const [mailAddress, setmailAddress] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginInfo, setloginInfo] = useState(false);
+  const [loginData, setloginData] = useState({
+    mailAddress: "",
+    password: "",
+  });
 
   const handleChange = (event) => {
-    switch (event.target.name) {
-      case "mailAddress":
-        setmailAddress(event.target.value);
-        break;
-      case "pass":
-        setPassword(event.target.value);
-        break;
-      default:
-        console.log("key not found");
-    }
+    const newLoginData = { ...loginData };
+    newLoginData[event.target.id] = event.target.value;
+    setloginData(newLoginData);
+    console.log(newLoginData);
+    event.preventDefault();
   };
+
+  useEffect(() => {
+    const submit = async () => {
+      await fetch(
+        axios
+          .post("http://153.127.48.168:8080/ecsite-api/user/login", {
+            setloginData,
+          })
+          //   本来ならここにif文でres=successが入る
+          .then((res) => {
+            console.log("success");
+            console.log(res.loginData);
+            setloginData(loginData.mailAddress, loginData.password);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      );
+      submit();
+    };
+  });
 
   return (
     <div>
@@ -27,28 +44,30 @@ export const Login = () => {
         <div className="container">
           <div className="row login-page">
             <div className="col s12 z-depth-6 card-panel"></div>
-            <form>
+            <form onSubmit={(event) => setloginData(event)}>
               <h1>ログイン画面 </h1>
-              <label htmlFor="mailAddress">メールアドレス</label>
+              <label htmlFor="mailAddress">メールアドレス:</label>
               <input
                 type="text"
                 className="mailAddress"
                 id="mailAddress"
-                onChange={handleChange}
+                onChange={(event) => handleChange(event)}
+                value={setloginData.mailAddress}
               />
               <br></br>
-              <label htmlFor="password">パスワード</label>
+              <label htmlFor="password">パスワード:</label>
               <input
                 type="text"
                 className="password"
                 id="password"
                 onChange={handleChange}
+                value={setloginData.password}
               />
               <div className="row login-btn">
                 <button
                   className="btn"
-                  type="button"
-                  onClick={() => setloginInfo(true)}
+                  type="submit"
+                  //   onClick={() => setloginData()}
                 >
                   ログイン
                 </button>
