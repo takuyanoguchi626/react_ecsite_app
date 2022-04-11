7e6d5ff7df0b09d7966d79f618cc2db3a8565fc5
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-04-11 10:47:29 +0900

    detail作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ItemDetail } from "./pages/ItemDetail";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ItemDetail></ItemDetail>
+    </div>
+  );
 }
 
 export default App;
