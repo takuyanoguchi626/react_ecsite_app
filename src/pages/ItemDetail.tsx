cdd0edb24a94faf7a60715f08ecf1086b69ba9d4
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-04-11 18:07:33 +0900

    itemDetail表示

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -87,25 +87,29 @@ export const ItemDetail = () => {
         );
       })}
 
-      <div>数量</div>
-      <select
-        name="quantity"
-        onChange={(e) => setQuantity(Number(e.target.value))}
-        defaultValue="1"
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-        <option value="11">11</option>
-        <option value="12">12</option>
-      </select>
+      <div>
+        数量
+        <select
+          className="quantity"
+          name="quantity"
+          id="pizzaQuantity"
+          // onChange={(e) => setQuantity(Number(e.target.value))}
+          defaultValue="1"
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+        </select>
+      </div>
 
       <div>この商品金額：3800円（税抜）</div>
       <div>
