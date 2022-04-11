2bfc41910c5256cd0f72e41189729ccd9d00952b
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-11 17:09:06 +0900

    入力内容をステートに格納

--- src/pages/OrderComfirm.tsx
+++ src/pages/OrderComfirm.tsx
@@ -1,113 +1,121 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import { UserInfo } from "../types/UserInfo";
+import axios from "axios";
+// import { format } from "date-fns";
 // import { CartListTable } from "../components/CartListTable";
 
-export function OrderComfirm() {
+export const OrderComfirm: FC = () => {
+  // const [userInfo, setUserInfo] = useState<UserInfo>({
+  //   name: "",
+  //   mailAddress: "",
+  //   zipCode: 0,
+  //   address: "",
+  //   telephone: 0,
+  //   deliveryDate: new Date(),
+  //   deliveryHour: 0,
+  //   paymentMethod: 0,
+  // });
+  const [name, setName] = useState("");
+  const [mailAddress, setMailAddress] = useState("");
+  const [zipcode, setZipcode] = useState(0);
+  const [address, setAddress] = useState("");
+  const [telephone, setTelephone] = useState(0);
+  const [deliveryDate, setDeliveryDate] = useState(new Date());
+  const [deliveryHour, setDeliveryHour] = useState(0);
+  const [paymentMethod, setPaymentMethod] = useState(0);
+
+  const [deliveryTime, setDeliveryTime] = useState("");
+
+  // format(
+  //   new Date(this.deliveryDate + "T" + this.deliveryHour + ":00:00"),
+  //   "yyyy/MM/dd HH:mm:ss"
+  // );
+
+  // useEffect(() => {
+  //   setUserInfo((userInfo: UserInfo) => {
+  //     return {
+  //       name: name,
+  //       mailAddress: mailAddress,
+  //       zipCode: zipcode,
+  //       address: address,
+  //       telephone: telephone,
+  //       deliveryDate: deliveryDate,
+  //       deliveryHour: deliveryHour,
+  //       paymentMethod: paymentMethod,
+  //     };
+  //   });
+  // }, [
+  //   name,
+  //   mailAddress,
+  //   zipcode,
+  //   address,
+  //   telephone,
+  //   deliveryDate,
+  //   deliveryHour,
+  //   paymentMethod,
+  // ]);
+
+  // console.log(userInfo);
+
+  const order = async () => {
+    const response = await axios.post(
+      `http://153.127.48.168:8080/ecsite-api/order`,
+      {
+        userId: 0, //仮
+        status: 0, //仮
+        totalPrice: 1000, //仮
+        destinationName: name,
+        destinationEmail: mailAddress,
+        destinationZipcode: zipcode,
+        destinationAddress: address,
+        destinationTel: telephone,
+        deliveryTime: deliveryTime,
+        paymentMethod: paymentMethod,
+        orderItemFormList: [], //仮
+      }
+    );
+  };
+
   return (
     <div>
       {/* <CartListTable></CartListTable> */}
       <div>
-        <h1>注文内容確認</h1>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>商品名</th>
-                <th>サイズ、価格(税抜)、数量</th>
-                <th>トッピング、価格(税抜)</th>
-                <th>小計</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <div>
-                    <img src="img/1.jpg" alt="" />
-                  </div>
-                  <span>ハワイアンパラダイス</span>
-                </td>
-                <td>
-                  <span>&nbsp;Ｌ</span>&nbsp;&nbsp;2,380円 &nbsp;&nbsp;1個
-                </td>
-                <td>
-                  <ul>
-                    <li>ピーマン300円</li>
-                    <li>オニオン300円</li>
-                    <li>あらびきソーセージ300円</li>
-                  </ul>
-                </td>
-                <td>
-                  <div>3,280円</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>
-                    <img src="img/1.jpg" alt="" />
-                  </div>
-                  <span>ハワイアンパラダイス</span>
-                </td>
-                <td>
-                  <span>&nbsp;Ｌ</span>&nbsp;&nbsp;2,380円 &nbsp;&nbsp;1個
-                </td>
-                <td>
-                  <ul>
-                    <li>ピーマン300円</li>
-                    <li>オニオン300円</li>
-                    <li>あらびきソーセージ300円</li>
-                  </ul>
-                </td>
-                <td>
-                  <div>3,280円</div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div>
-                    <img src="img/1.jpg" alt="" />
-                  </div>
-                  <span>ハワイアンパラダイス</span>
-                </td>
-                <td>
-                  <span>&nbsp;Ｌ</span>&nbsp;&nbsp;2,380円 &nbsp;&nbsp;1個
-                </td>
-                <td>
-                  <ul>
-                    <li>ピーマン300円</li>
-                    <li>オニオン300円</li>
-                    <li>あらびきソーセージ300円</li>
-                  </ul>
-                </td>
-                <td>
-                  <div>3,280円</div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div>
-          <div>消費税：8,000円</div>
-          <div>ご注文金額合計：38,000円 (税込)</div>
-        </div>
-
         <h2>お届け先情報</h2>
         <div>
           <div>
             <div>
-              <input id="name" type="text" />
               <label htmlFor="name">お名前</label>
+              <input
+                id="name"
+                type="text"
+                onChange={(e) => {
+                  setName(() => e.target.value);
+                }}
+              />
             </div>
           </div>
           <div>
             <div>
-              <input id="email" type="email" />
               <label htmlFor="email">メールアドレス</label>
+              <input
+                id="email"
+                type="email"
+                onChange={(e) => {
+                  setMailAddress(() => e.target.value);
+                }}
+              />
             </div>
           </div>
           <div>
             <div>
-              <input id="zipcode" type="text" />
               <label htmlFor="zipcode">郵便番号(ハイフンなし)</label>
+              <input
+                id="zipcode"
+                type="number"
+                onChange={(e) => {
+                  setZipcode(() => Number(e.target.value));
+                }}
+              />
               <button type="button">
                 <span>住所検索</span>
               </button>
@@ -115,55 +123,136 @@ export function OrderComfirm() {
           </div>
           <div>
             <div>
-              <input id="address" type="text" />
               <label htmlFor="address">住所</label>
+              <input
+                id="address"
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setAddress(() => e.target.value)
+                }
+              />
             </div>
           </div>
           <div>
             <div>
-              <input id="tel" type="tel" />
               <label htmlFor="tel">電話番号</label>
+              <input
+                id="tel"
+                type="tel"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setTelephone(() => Number(e.target.value))
+                }
+              />
             </div>
           </div>
           <div>
             <div>
-              <input id="deliveryDate" type="date" />
               <label htmlFor="address">配達日時</label>
+              <input
+                id="deliveryDate"
+                type="date"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryDate(() => new Date(e.target.value))
+                }
+              />
             </div>
             <label>
-              <input name="deliveryTime" type="radio" value="10時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="10"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>10時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="11時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="11"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>11時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="12時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="12"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>12時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="13時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="13"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>13時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="14時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="14"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>14時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="15時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="15"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>15時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="16時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="16"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>16時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="17時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="17"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>17時</span>
             </label>
             <label>
-              <input name="deliveryTime" type="radio" value="18時" />
+              <input
+                name="deliveryTime"
+                type="radio"
+                value="18"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDeliveryHour(() => Number(e.target.value))
+                }
+              />
               <span>18時</span>
             </label>
           </div>
@@ -173,11 +262,25 @@ export function OrderComfirm() {
         <div>
           <span>
             <label>
-              <input name="paymentMethod" type="radio" value="1" />
+              <input
+                name="paymentMethod"
+                type="radio"
+                value="1"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPaymentMethod(() => Number(e.target.value))
+                }
+              />
               <span>代金引換</span>
             </label>
             <label>
-              <input name="paymentMethod" type="radio" value="2" />
+              <input
+                name="paymentMethod"
+                type="radio"
+                value="2"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPaymentMethod(() => Number(e.target.value))
+                }
+              />
               <span>クレジットカード</span>
             </label>
           </span>
@@ -190,4 +293,4 @@ export function OrderComfirm() {
       </div>
     </div>
   );
-}
+};
