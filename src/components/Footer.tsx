0158cba3da7df2fc26649557cf67aeab35eb1b5e
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-04-15 16:50:51 +0900

    footerのｃｓｓを整えました

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Grid } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
+import "../css/footer.css";
 
 export const Footer = () => {
   const navigate = useNavigate();
   return (
-    // <Box sx={{ flexGrow: 1 }}>
-    <AppBar
-      position="static"
-      sx={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-    >
-      <Toolbar>
-        <Grid container spacing={8}>
-          <Grid item xs={4} md={2}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ color: "#e0f2f1", backgroundColor: "#004d40", height: 300 }}
+      >
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={4} md={2} className="grid">
             <div>ヘルプ</div>
             <div> カロリー情報</div>
             <div>アレルギー情報</div>
             <div>注文する</div>
-            <div>ご注文方法</div>
-            <div>お問い合わせ</div>
-            <div>FAQ</div>
-            <div>サイトマップ</div>
           </Grid>
           <Grid item xs={4} md={2}>
             <div>ヘルプ</div>
@@ -57,8 +54,42 @@ export const Footer = () => {
             ></img>
           </Grid>
         </Grid>
-      </Toolbar>
-    </AppBar>
-    // </Box>
+        <AppBar position="static" sx={{ backgroundColor: "#f0e68c" }}>
+          <div className="iconList">
+            <span className="iconWraper">
+              <img
+                className="creditIcon"
+                src="/VISAロゴデータ-1.png"
+                alt="クレジットカード"
+              ></img>
+            </span>
+            <span className="iconWraper">
+              <img
+                className="creditIcon"
+                src="/jcb.png"
+                alt="クレジットカード"
+              ></img>
+            </span>
+            <span className="iconWraper">
+              <img
+                className="creditIcon"
+                src="/MASTERCARDロゴデータ.png"
+                alt="クレジットカード"
+              ></img>
+            </span>
+            <span className="iconWraper">
+              <img
+                className="creditIcon"
+                src="/americanexpress.jpg"
+                alt="クレジットカード"
+              ></img>
+            </span>
+          </div>
+        </AppBar>
+        <div className="copyright">
+          Copyright Domino's Pizza Japan, Inc. All Rights Reserved.
+        </div>
+      </AppBar>
+    </Box>
   );
 };
