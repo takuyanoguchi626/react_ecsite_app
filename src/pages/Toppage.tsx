0d93567d568f3ddd678648ca8896509c8e18f471
kanacoyn <knkyn723@gmail.com>
2022-04-20 17:54:42 +0900

    参考サイトのURLと文章を拝借

--- src/pages/Toppage.tsx
+++ src/pages/Toppage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import { Card, CardMedia, Grid } from "@material-ui/core";
+import { Box, CardMedia, Grid } from "@material-ui/core";
 
 export const Toppage = () => {
   const navigate = useNavigate();
-
+  // 参考サイト
+  // http://www.salvatore.jp/restaurant/daikanyama/
+  // http://www.da-isa.jp/
   return (
     <div>
       <Grid
@@ -14,40 +16,64 @@ export const Toppage = () => {
         justifyContent="center"
         alignItems="flex-start"
       >
-        <span className="container">
-          <Grid container justifyContent="center" alignItems="flex-start">
-            <h1>Enjoy our Delicious Pizza</h1>
-          </Grid>
-
-          <Grid container justifyContent="center" alignItems="flex-start">
-            <CardMedia
-              component="img"
-              height="500"
-              src="../img_pizza/pizza_toppage.png"
-            />
-            背景を画像にしたりなんやらかんやらする
-          </Grid>
-
-          <div>
-            <Button
-              className="btn"
-              style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-              variant="contained"
-              onClick={() => navigate("/login")}
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <h1>Enjoy our Delicious Pizza</h1>
+        </Grid>
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <CardMedia
+            component="img"
+            height="500"
+            src="../img_pizza/pizza_toppage.png"
+          />
+          <h3>
+            ナポリピッツァを日本に広めた功労者
+            サルヴァトーレ・クオモがプロデュースするピッツェリア
+          </h3>
+          イタリアの窯職人が作ったピッツァ窯の中で焼き上げるナポリピッツァは、薄生地なのにもちもちで、薪のかおりが香ばしい逸品です。
+          <br></br>
+          ご家族、仲間同士のディナーやパーティに、わいわい楽しくイタリアンスタイルでお楽しみ下さい。
+        </Grid>
+        <div>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={2}
+              justifyContent="center"
+              alignItems="flex-start"
             >
-              ログインしてからオーダーを開始する
-            </Button>
-            &nbsp; &nbsp;
-            <Button
-              className="btn"
-              style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-              variant="contained"
-              onClick={() => navigate("/registerUser")}
-            >
-              会員登録する
-            </Button>
-          </div>
-        </span>
+              <Grid item xs={8}>
+                <Button
+                  className="btn"
+                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                  variant="contained"
+                  onClick={() => navigate("/login")}
+                >
+                  ログインしてからオーダーを開始する
+                </Button>
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  className="btn"
+                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                  variant="contained"
+                  onClick={() => navigate("/registerUser")}
+                >
+                  会員登録する
+                </Button>
+              </Grid>
+              <Grid item xs={10}>
+                <Button
+                  className="btn"
+                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                  variant="contained"
+                  onClick={() => navigate("/itemList")}
+                >
+                  メニューを見る
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </div>
       </Grid>
     </div>
   );
