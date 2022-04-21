ab66c7d94a511b3db6b733e265dbf13328bc5871
kanacoyn <knkyn723@gmail.com>
2022-04-21 16:02:32 +0900

    ボタンの配置を修正

--- src/pages/Toppage.tsx
+++ src/pages/Toppage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import { Box, CardMedia, Grid } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
 export const Toppage = () => {
   const navigate = useNavigate();
@@ -25,55 +32,84 @@ export const Toppage = () => {
             height="500"
             src="../img_pizza/pizza_toppage.png"
           />
-          <h3>
-            ナポリピッツァを日本に広めた功労者
-            サルヴァトーレ・クオモがプロデュースするピッツェリア
-          </h3>
-          イタリアの窯職人が作ったピッツァ窯の中で焼き上げるナポリピッツァは、薄生地なのにもちもちで、薪のかおりが香ばしい逸品です。
-          <br></br>
-          ご家族、仲間同士のディナーやパーティに、わいわい楽しくイタリアンスタイルでお楽しみ下さい。
         </Grid>
-        <div>
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid
-              container
-              spacing={2}
-              justifyContent="center"
-              alignItems="flex-start"
-            >
-              <Grid item xs={8}>
-                <Button
-                  className="btn"
-                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-                  variant="contained"
-                  onClick={() => navigate("/login")}
-                >
-                  ログインしてからオーダーを開始する
-                </Button>
-              </Grid>
-              <Grid item xs={4}>
-                <Button
-                  className="btn"
-                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-                  variant="contained"
-                  onClick={() => navigate("/registerUser")}
-                >
-                  会員登録する
-                </Button>
-              </Grid>
-              <Grid item xs={10}>
-                <Button
-                  className="btn"
-                  style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
-                  variant="contained"
-                  onClick={() => navigate("/itemList")}
-                >
-                  メニューを見る
-                </Button>
-              </Grid>
+        <br></br>
+
+        <Box sx={{ flexGrow: 0 }} pt={2} pb={4}>
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            justifyContent="center"
+            alignItems="flex-start"
+          >
+            <Grid item xs={6}>
+              <Button
+                className="btn"
+                style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                variant="contained"
+                onClick={() => navigate("/login")}
+              >
+                ログインしてからオーダーを開始する
+              </Button>
             </Grid>
+            <Grid item xs={4}>
+              <Button
+                className="btn"
+                style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                variant="contained"
+                onClick={() => navigate("/registerUser")}
+              >
+                会員登録する
+              </Button>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant="h6">
+                <div>
+                  ﾟ･*:.｡. .｡.:*･゜まずはここからチェックﾟ･*:.｡. .｡.:*･゜ﾟ
+                </div>
+              </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <Button
+                className="btn"
+                style={{ color: "#e0f2f1", backgroundColor: "#004d40" }}
+                variant="contained"
+                onClick={() => navigate("/itemList")}
+              >
+                メニューを見る
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+        <br />
+        <Grid xs={10} spacing={2} direction="row">
+          <Box sx={{ mt: 2, pt: 2, marginBottom: 2 }}>
+            <Card>
+              <Grid xs={2}>
+                <CardMedia
+                  component="img"
+                  height="20%"
+                  src="../italianMan.png"
+                />
+              </Grid>
+              <CardContent>
+                <Grid xs={8}>
+                  <Typography variant="h5">
+                    ナポリピッツァを日本に広めた功労者 <br></br>
+                    サルヴァトーレ・クオモがプロデュースするピッツェリア
+                  </Typography>
+                  <Typography variant="h6">
+                    イタリアの窯職人が作ったピッツァ窯の中で焼き上げるナポリピッツァは、薄生地なのにもちもちで、薪のかおりが香ばしい逸品です。
+                    <br></br>
+                    ご家族、仲間同士のディナーやパーティに、わいわい楽しくイタリアンスタイルでお楽しみ下さい。
+                  </Typography>
+                </Grid>
+              </CardContent>
+            </Card>
           </Box>
-        </div>
+        </Grid>
+        <div></div>
       </Grid>
     </div>
   );
