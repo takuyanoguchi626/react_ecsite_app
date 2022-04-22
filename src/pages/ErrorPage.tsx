39af207da8fabd3f5f2340266ffb04eab194ff86
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-04-22 15:09:32 +0900

    エラーページ修正

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,19 +9,19 @@ export const ErrorPage = () => {
       <Grid container justifyContent="center" alignItems="flex-start">
         <span className="errorImage">
           <br />
-          <span className="errorMessage">
-            <p>404エラー </p>
-            <p>申し訳ありません、ページが見つかりませんでした…</p>
-            <br />
-            <img
-              src="img/error_image.png"
-              alt="test"
-              style={{
-                width: "300px",
-                height: "300px",
-              }}
-            />
-          </span>
+        </span>
+        <span className="errorMessage">
+          <p>404エラー </p>
+          <p>申し訳ありません、ページが見つかりませんでした…</p>
+          <br />
+          <img
+            src="img/error_image.png"
+            alt="test"
+            style={{
+              width: "300px",
+              height: "300px",
+            }}
+          />
         </span>
       </Grid>
     </div>
