61711a64a8bf8e0c3c898d8a68581b0df5ae588d
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-04-26 13:11:25 +0900

    DBへ問い合わせ情報を送信

--- src/pages/QuestionForm.tsx
+++ src/pages/QuestionForm.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import axios from "axios";
+import { app } from "../app/config";
+import { collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 export const QuestionForm = () => {
+  // firestore認証
+  const db = getFirestore(app);
+  const docRef = collection(db, "questionForm");
+
+  //   ユーザー情報
+  const [userName, setUserName] = useState("");
+  const [userAddress, setUserAddress] = useState("");
+  const [userId, setUserId] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   ユーザー情報をfirebaseに送る
+  const sendUserInfo = async () => {
+    try {
+      await addDoc(docRef, {
+        name: userName,
+        address: userAddress,
+        userId: userId,
+        message: message,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(db);
+  console.log(collection(db, "questionForm"));
+
   return (
     <div style={{ textAlign: "center" }}>
       <form>
@@ -9,15 +41,30 @@ export const QuestionForm = () => {
         <div>
           お名前
           <br />
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setUserName(e.target.value)}
+          />
           <br />
           住所
           <br />
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="address"
+            id="address"
+            onChange={(e) => setUserAddress(e.target.value)}
+          />
           <br />
           ユーザーID
           <br />
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="userId"
+            id="userId"
+            onChange={(e) => setUserId(e.target.value)}
+          />
           <br />
           お問い合わせ内容
           <br />
@@ -26,9 +73,10 @@ export const QuestionForm = () => {
             id="questionform"
             cols={30}
             rows={10}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
           <br />
-          <Button>送信</Button>
+          <Button onClick={sendUserInfo}>送信</Button>
         </div>
       </form>
     </div>
