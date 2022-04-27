cad01828e554cbd129614b5ba039caca93bffdca
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-04-27 12:02:57 +0900

    footerにリンクを貼る

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import { Grid, Link } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import "../css/footer.css";
-
+import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <AppBar
@@ -11,43 +11,47 @@ export const Footer = () => {
       sx={{ color: "#e0f2f1", backgroundColor: "#004d40", height: 370 }}
     >
       <Box sx={{ flexGrow: 0 }} pt={2} pb={4}>
-        <Grid
-          container
-          spacing={0}
-          direction="row"
-          justifyContent="center"
-          alignItems="flex-start"
-        >
-          <Grid item xs={4} md={2} className="grid">
-            <div>ヘルプ</div>
-            <div> カロリー情報</div>
-            <div>アレルギー情報</div>
-            <div>注文する</div>
+        <div className="appBar">
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            justifyContent="center"
+            alignItems="flex-start"
+          >
+            <Grid item xs={4} md={2} className="grid">
+              <div>ヘルプ</div>
+              <div> カロリー情報</div>
+              <div>アレルギー情報</div>
+              <div>注文する</div>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <div>ヘルプ</div>
+              <div> カロリー情報</div>
+              <div>アレルギー情報</div>
+              <div>注文する</div>
+              <div>ご注文方法</div>
+              <div>
+                <Link to="/QuestionForm">お問い合わせ</Link>
+              </div>
+              <div>FAQ</div>
+              <div>サイトマップ</div>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <div>ヘルプ</div>
+              <div> カロリー情報</div>
+              <div>アレルギー情報</div>
+              <div>注文する</div>
+              <div>ご注文方法</div>
+              <div>お問い合わせ</div>
+              <div>FAQ</div>
+              <div>サイトマップ</div>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <img src="../img_pizza/header_logo.png" alt="PIZZA"></img>
+            </Grid>
           </Grid>
-          <Grid item xs={4} md={2}>
-            <div>ヘルプ</div>
-            <div> カロリー情報</div>
-            <div>アレルギー情報</div>
-            <div>注文する</div>
-            <div>ご注文方法</div>
-            <div>お問い合わせ</div>
-            <div>FAQ</div>
-            <div>サイトマップ</div>
-          </Grid>
-          <Grid item xs={4} md={2}>
-            <div>ヘルプ</div>
-            <div> カロリー情報</div>
-            <div>アレルギー情報</div>
-            <div>注文する</div>
-            <div>ご注文方法</div>
-            <div>お問い合わせ</div>
-            <div>FAQ</div>
-            <div>サイトマップ</div>
-          </Grid>
-          <Grid item xs={4} md={2}>
-            <img src="../img_pizza/header_logo.png" alt="PIZZA"></img>
-          </Grid>
-        </Grid>
+        </div>
       </Box>
       <Box sx={{ flexGrow: 0 }} pt={5}>
         <AppBar position="static" sx={{ backgroundColor: "#f0e68c" }}>
