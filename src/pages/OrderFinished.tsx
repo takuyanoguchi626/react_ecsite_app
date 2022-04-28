a5542aa468724774f92e282e6e990f06fc30963f
kanacoyn <knkyn723@gmail.com>
2022-04-28 09:45:52 +0900

    注文完了画面を作成。画面からトップもしくは注文履歴へ促すページ

--- src/pages/OrderFinished.tsx
+++ src/pages/OrderFinished.tsx
@@ -1,5 +1,36 @@
+import { Button, Typography } from "@material-ui/core";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/orderFinished.css";
 
 export const OrderFinished = () => {
-  return <div>OrderFinished</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="completeOrder">
+      <Typography variant="h3">決済が完了しました！</Typography>
+      <Typography variant="h5">この度はご注文ありがとうございます。</Typography>
+
+      <p>
+        お支払い先は、お送りしたメールに記載してありますのでご確認ください。
+      </p>
+      <p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+
+      <Button
+        className="btn"
+        style={{ fontWeight: 700 }}
+        onClick={() => navigate("/")}
+      >
+        トップ画面に戻る
+      </Button>
+
+      <Button
+        className="btn"
+        style={{ fontWeight: 700 }}
+        onClick={() => navigate("/orderHistory")}
+      >
+        注文履歴
+      </Button>
+    </div>
+  );
 };
